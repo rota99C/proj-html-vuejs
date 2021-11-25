9c54eb49b3b53c9c1e7caa9e41c4c9540ab09f69
rota99C <rotaboolean@gmail.com>
2021-11-25 16:55:13 +0100

    header completo e stilizzato con menu popolato  dinamicamente

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,9 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,10 +1,54 @@
 <template>
-  <div id="SiteHeader"></div>
+  <div id="SiteHeader" class="w-100 d-flex justify-content-between">
+    <img src="../assets/img/avada-charity-logo.png" alt="logo-AvadaCharity" />
+    <div class="navbar">
+      <ul>
+        <li v-for="menuItem in menuItems" :key="menuItem" class="d-inline">
+          <a href="" class="text-decoration-none">
+            {{ menuItem }}
+          </a>
+        </li>
+        <button class="golden_button">
+          <a href="" class="text-decoration-none">DONATE</a>
+        </button>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      menuItems: ["HOME", "MISSION", "CAUSES", "JOURNAL"],
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/style/variables.scss";
+#SiteHeader {
+  height: 150px;
+  padding: 45px 85px;
+  li {
+    margin-right: 40px;
+    color: $white;
+    a:visited {
+      color: $white;
+    }
+    a:hover {
+      color: $golden;
+    }
+  }
+  .golden_button {
+    background-color: $golden;
+    border-style: none;
+    padding: 8px 29px;
+    color: $white;
+    a:visited {
+      color: $white;
+    }
+  }
+}
 </style>
