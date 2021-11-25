6170c6ff4a7e9edffb2b82e263ac03af2e5db3e5
rota99C <rotaboolean@gmail.com>
2021-11-25 15:35:06 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SiteHeader />
+    <SiteJumbo />
+    <SiteMain />
+    <SiteFooter />
+  </div>
+</template>
+
+<script>
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteJumbo from "./components/SiteJumbo.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
+export default {
+  name: "App",
+  components: {
+    SiteHeader,
+    SiteJumbo,
+    SiteMain,
+    SiteFooter,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/SiteJumbo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="SiteJumbo"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
