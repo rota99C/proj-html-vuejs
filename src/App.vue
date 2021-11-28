6718e09caee0d7a3f67298dcdac9000f8c722fce
rota99C <rotaboolean@gmail.com>
2021-11-28 17:35:23 +0100

    creato funzione per lo scorrimento della pagina e finito definitivamente l'esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="bottone_goUp" v-if="visible">
+      <a href="#app"><font-awesome-icon :icon="['fas', 'chevron-up']" /></a>
+    </div>
     <Slideshow />
     <SiteMain />
     <SiteFooter />
@@ -10,20 +13,60 @@
 import Slideshow from "./components/Slideshow.vue";
 import SiteMain from "./components/SiteMain.vue";
 import SiteFooter from "./components/SiteFooter.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
   name: "App",
   components: {
     Slideshow,
     SiteMain,
     SiteFooter,
+    FontAwesomeIcon,
+  },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    createButton() {
+      if (window.scrollY > 100) {
+        //console.log("string");
+        this.visible = true;
+      } else {
+        this.visible = false;
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.createButton);
+    /* function goBack() {
+      if (window.scrollY > 100) {
+        this.visible = true;
+      }
+    } */
   },
 };
 </script>
 
 <style lang="scss">
+@import "./assets/style/common.scss";
 * {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+.bottone_goUp {
+  width: 50px;
+  height: 40px;
+  background-color: $thunder;
+  position: fixed;
+  bottom: 0;
+  right: 120px;
+  text-align: center;
+  line-height: 40px;
+  .svg-inline--fa {
+    font-size: 20px;
+    color: $white;
+  }
 }
 </style>
